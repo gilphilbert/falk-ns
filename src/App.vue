c206b4a2d2bb482966b36c1e3a3bdd989d7b87f1
gilphilbert <gil.philbert@gmail.com>
2021-05-03 21:38:02 -0700

    welcome

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,46 @@
 <template>
   <div id="app">
     <Login v-if="loginScreen" :isLoggedIn="isLoggedIn" />
+    <Welcome v-if="welcomeScreen" @complete="welcomeComplete" />
     <Main v-if="showApp" :isLoggedIn="isLoggedIn" />
   </div>
 </template>
 <script>
 import Login from './components/Login.vue'
+import Welcome from './components/Welcome.vue'
 import Main from './components/Main.vue'
 export default {
   name: 'App',
   components: {
     Login,
+    Welcome,
     Main
   },
   created () {
     window.fetch('/api/check')
       .then(r => {
-        let loggedIn = false
         if (r.status === 200) {
-          loggedIn = true
+          console.log('App')
+          this.loginScreen = false
+          this.welcomeScreen = false
+          this.showApp = true
+        } else if (r.status === 400) {
+          console.log('Welcome')
+          this.loginScreen = false
+          this.welcomeScreen = true
+          this.showApp = false
+        } else {
+          console.log('Login')
+          this.loginScreen = true
+          this.welcomeScreen = false
+          this.showApp = false
         }
-        this.loginScreen = !loggedIn
-        this.showApp = loggedIn
       })
   },
   data () {
     return {
       loginScreen: false,
+      welcomeScreen: false,
       showApp: false
     }
   },
@@ -35,6 +49,10 @@ export default {
       this.loginScreen = !state
       this.showApp = state
       this.$database.update()
+    },
+    welcomeComplete () {
+      this.welcomeScreen = false
+      this.loginScreen = true
     }
   }
 }
--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -6,19 +6,19 @@
       <h1>Welcome!</h1>
       <p class="subtitle is-3">It looks like you're new around here</p><p class="is-6 has-text-centered">Before you can log in you'll need to set the admin account password<br><br></p>
       <fieldset style="margin: 5px 0; max-width: 300px; margin: 0 auto">
-        <input style="background-color: #404040; border-color: #404040" data-bind="value: username" class="input" type="text" readonly />
+        <input style="background-color: #404040; border-color: #404040" :value="user" class="input" type="text" readonly />
         <p class="small"></p>
       </fieldset>
       <fieldset style="margin: 5px 0; max-width: 300px; margin: 0 auto">
-        <input data-bind="value: password" class="input" type="password" placeholder="Password" autofocus="true" />
-        <p class="has-text-danger" data-bind="text: passwordMessage"></p>
+        <input v-model="pass" class="input" type="password" placeholder="Password" />
+        <p class="has-text-danger">{{ passwordMessage }}</p>
       </fieldset>
       <fieldset style="margin: 5px 0; max-width: 300px; margin: 0 auto">
-        <input data-bind="value: vpassword" class="input" type="password" placeholder="Verify password" />
-        <p class="has-text-danger" data-bind="text: vpasswordMessage"></p>
+        <input v-model="passV" class="input" type="password" placeholder="Verify password" />
+        <p class="has-text-danger">{{ passwordVMessage }}</p>
       </fieldset>
       <fieldset style="margin: 5px 0">
-        <button class="button is-rounded is-primary" data-bind="click: setPassword">Set Password</button>
+        <button class="button is-rounded is-primary" @click="setPassword">Set Password</button>
       </fieldset>
     </div>
   </div>
@@ -30,29 +30,45 @@ export default {
   props: [ 'isLoggedInParent' ],
   data() {
     return {
-      user: '',
+      user: 'admin',
       pass: '',
-      error: '',
+      passV: '',
+      passwordMessage: '',
+      passwordVMessage: ''
     }
   },
   methods: {
-    login() {
+    setPassword () {
+      let err = false
+      if (this.pass.length < 8) {
+        this.passwordMessage = 'Passwords must be at least 8 characters long'
+        err = true
+      } else {
+        this.passwordMessage = ''
+      }
+      if (this.pass !== this.passV) {
+        this.passwordVMessage = 'Passwords do not match'
+        err = true
+      } else {
+        this.passwordVMessage = ''
+      }
+      if (err) {
+        return
+      }
+      this.passwordMessage = ''
+      this.passwordVMessage = ''
       const body = JSON.stringify({ username: this.user, password: this.pass })
-      window.fetch('/api/login', { method: 'post', headers: { Accept: 'application/json', 'Content-Type': 'application/json' }, body: body })
+      window.fetch('/api/welcome', { method: 'post', headers: { Accept: 'application/json', 'Content-Type': 'application/json' }, body: body })
         .then(response => response.json())
-        .then(data => {
-          if (data.state === 'failed' || data.state === false) {
-            // show a message that login failed
-            this.error = 'Invalid username and/or password'
+        .then((data) => {
+          if (data.state) {
+            this.$emit('complete')
           } else {
-            this.isLoggedInParent()
+            // no reason we should actually get here, only if someone's trying to hack us...
+            console.log('failed... nice try')
           }
         })
-        .catch(err => {
-          console.log(err)
-        })
-    }
+      }
   }
 }
 </script>
-<style scoped></style>
